d214d2bc665fa01fd0a36bd61e34060f74dc7ae1
chris48s <chris.shaw480@gmail.com>
2020-08-15 21:45:31 +0100

    auto-close pointless gatsby bumps

---
+++ helpers.js
@@ -0,0 +1,56 @@
+function findChangelogStart(lines) {
+  for (let i = 0; i < lines.length; i++) {
+    line = lines[i];
+    if (
+      line === "<summary>Changelog</summary>" &&
+      lines[i + 2] === "<blockquote>"
+    ) {
+      return i + 3;
+    }
+  }
+  return null;
+}
+
+function findChangelogEnd(lines, start) {
+  for (let i = start; i < lines.length; i++) {
+    line = lines[i];
+    if (line === "</blockquote>") {
+      return i;
+    }
+  }
+  return null;
+}
+
+function allChangelogLinesAreVersionBump(changelogLines) {
+  return (
+    changelogLines.length > 0 &&
+    changelogLines.length ===
+      changelogLines.filter((line) =>
+        line.includes("Version bump only for package")
+      ).length
+  );
+}
+
+function isPointlessGatsbyBump(body) {
+  const lines = body.split(/\r?\n/);
+  if (!lines[0].includes("https://github.com/gatsbyjs/gatsby")) {
+    return false;
+  }
+  const start = findChangelogStart(lines);
+  const end = findChangelogEnd(lines, start);
+  if (!start || !end) {
+    return false;
+  }
+  const changelogLines = lines
+    .slice(start, end)
+    .filter((line) => !line.startsWith("<h"));
+  return allChangelogLinesAreVersionBump(changelogLines);
+}
+
+function shouldAutoMerge(body) {
+  return body.includes(
+    "If all status checks pass Dependabot will automatically merge this pull request"
+  );
+}
+
+module.exports = { isPointlessGatsbyBump, shouldAutoMerge };
